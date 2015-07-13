f875833f78559c39553fd3f568502e50466fc0b7
Neioumi <neioumi@me.com>
2015-07-13 20:35:52 +0900

    Initial commit :octocat:

---
+++ DeapOnesHunt/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  DeapOnesHunt
+//
+//  Created by biyo on 2015/07/13.
+//  Copyright (c) 2015年 Naomi Hino. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ DeapOnesHunt/GameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GameViewController.swift
+//  DeapOnesHunt
+//
+//  Created by biyo on 2015/07/13.
+//  Copyright (c) 2015年 Naomi Hino. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let scene = GameScene(size:CGSize(width: 750, height: 1334))
+        let skView = self.view as! SKView
+        scene.scaleMode = .AspectFill
+        skView.presentScene(scene)
+    }
+
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> Int {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+        } else {
+            return Int(UIInterfaceOrientationMask.All.rawValue)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
